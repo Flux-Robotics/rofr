1bc16284736ae343db9919a3cb8b2995ae6a9cc1
Liam Kinne <liam@kinne.io>
2026-04-17 04:09:59 +1000

    constants lifetime is static by default

--- nodal/src/header.rs
+++ nodal/src/header.rs
@@ -1,6 +1,6 @@
 /// Service Nodal version header.
-pub const SERVICE_VERSION: &'static str = "Nodal-Version";
+pub const SERVICE_VERSION: &str = "Nodal-Version";
 /// Endpoint request schema header.
-pub const ENDPOINT_REQUEST_SCHEMA: &'static str = "Nodal-Request-Schema";
+pub const ENDPOINT_REQUEST_SCHEMA: &str = "Nodal-Request-Schema";
 /// Endpoint response schema header.
-pub const ENDPOINT_RESPONSE_SCHEMA: &'static str = "Nodal-Response-Schema";
+pub const ENDPOINT_RESPONSE_SCHEMA: &str = "Nodal-Response-Schema";
