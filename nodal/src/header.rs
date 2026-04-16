882de45428bb5987ff40d27cf238ff95fb77b493
Liam Kinne <liam@kinne.io>
2026-04-17 04:11:19 +1000

    shorten names

--- nodal/src/header.rs
+++ nodal/src/header.rs
@@ -1,6 +1,6 @@
 /// Service Nodal version header.
-pub const SERVICE_VERSION: &str = "Nodal-Version";
+pub const VERSION: &str = "Nodal-Version";
 /// Endpoint request schema header.
-pub const ENDPOINT_REQUEST_SCHEMA: &str = "Nodal-Request-Schema";
+pub const REQUEST_SCHEMA: &str = "Nodal-Request-Schema";
 /// Endpoint response schema header.
-pub const ENDPOINT_RESPONSE_SCHEMA: &str = "Nodal-Response-Schema";
+pub const RESPONSE_SCHEMA: &str = "Nodal-Response-Schema";
