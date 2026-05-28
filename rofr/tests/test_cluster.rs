78876435445b81d08883aed21d1e5d10a079ddfa
Liam Kinne <liam@kinne.io>
2026-05-29 03:16:16 +1000

    add test with custom connect options

--- rofr/tests/test_cluster.rs
+++ rofr/tests/test_cluster.rs
@@ -1,5 +1,6 @@
 use std::time::Duration;
 
+use async_nats::ConnectOptions;
 use rofr::Cluster;
 
 #[tokio::test]
@@ -12,5 +13,19 @@ async fn cluster_no_services() {
     assert!(
         result.is_err(),
         "cluster without services exited immediately"
+    );
+}
+
+#[tokio::test]
+async fn cluster_with_connect_options() {
+    let server = nats_server::run_server("tests/nats/default.conf");
+
+    let options = ConnectOptions::new().name("rofr-cluster-with-connect-options");
+    let cluster = Cluster::new_with_options(server.client_url(), options).unwrap();
+
+    let result = tokio::time::timeout(Duration::from_millis(50), cluster.run()).await;
+    assert!(
+        result.is_err(),
+        "cluster with connect options exited immediately"
     );
 }
